0e00682b86f71ee26383aa97b266ef26712fbf53
Deepak-San <deepakumarshay@gmail.com>
2025-11-19 03:35:04 +0530

    Add files via upload

---
+++ models/Product.js
@@ -0,0 +1,13 @@
+
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  price: { type: Number, required: true },
+  image: String,
+  category: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
